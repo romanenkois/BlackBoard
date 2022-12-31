93f1a7ad440d88e66ad66a0ca84b0aad08dbaafb
Illya <romanenkoisnot@gmail.com>
2022-12-31 20:26:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 
 
 function get_menu() {
-    if (document.getElementById("navmenu").style.display=="block") {
-        document.getElementById("navmenu").style.display="none"
-    }
-    else {
+    var filterExists = document.getElementById("filter");
+    if(filterExists == null){
+        if (document.getElementById("navmenu").style.display=="block") {
+            document.getElementById("navmenu").style.display="none"
+        }
+        else {
 
-        document.getElementById("navmenu").style.display="block"
+            document.getElementById("navmenu").style.display="block"
+        }
+    } else {
+        if (document.getElementById("filter").style.display=="block") {
+            document.getElementById("filter").style.display="none"
+        } else {
+            document.getElementById("filter").style.display="block"
+        }
     }
 }
