570328081f8eddb8b10bd539e76926b05b2391f7
Illya <romanenkoisnot@gmail.com>
2023-01-17 20:24:28 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,3 +1,9 @@
+
+function addProduct(productId, productName, productPrice) {
+    document.getElementById("products").insertAdjacentHTML("beforeend",
+        `<div class="col-lg-4 col-6 product"><a href="product.html?${productId}"><img class="promo" src="images/promos/promo3.png" alt="image"></a><p class="product-name">${productName}</p><p class="product-price">${productPrice} грн</p></div>`);
+}
+
 function addProductType(productType) {
     document.getElementById("products").innerHTML = "";
         fetch("products.json")
@@ -5,16 +11,11 @@ function addProductType(productType) {
         .then(json => {
             for (let i = 0; i < json.products.length; i++) {
                 if (json.products[i].product_type == productType) {
-                    addProduct(json.products[i].name, json.products[i].price);
+                    addProduct(json.products[i].product_id, json.products[i].name, json.products[i].price);
                 }
             }
         }
     );
-}
-
-function addProduct(productName, productPrice) {
-    document.getElementById("products").insertAdjacentHTML("beforeend",
-        `<div class="col-lg-4 col-6 product"><a href="product.html"><img class="promo" src="images/promos/promo3.png" alt="image"></a><p class="product-name">${productName}</p><p class="product-price">${productPrice} грн</p></div>`);
 }
 
 function addAllProducts() {
@@ -23,7 +24,7 @@ function addAllProducts() {
         .then(response => response.json())
         .then(json => {
             for (let i = 0; i < json.products.length; i++) {
-                addProduct(json.products[i].name, json.products[i].price);
+                addProduct(json.products[i].product_id, json.products[i].name, json.products[i].price);
             }
         }
     );
