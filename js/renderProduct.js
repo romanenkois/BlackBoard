897726563e0bfa196c9dc77e1452e76a8dfc4b3d
Illya <romanenkoisnot@gmail.com>
2023-01-17 23:14:06 +0200

    Update renderProduct.js

--- js/renderProduct.js
+++ js/renderProduct.js
@@ -1,12 +1,49 @@
 var productId = (location.href.substring(location.href.lastIndexOf('?') + 1));
 
 function checkIfProductExists(productId) {
-    var product = products.find(function (product) {
-        return product.id === productId;
+    var productExists = false;
+    fetch("products.json")
+    .then(response => response.json())
+    .then(json => {
+        for (let i = 0; i < json.products.length; i++) {
+            if (json.products[i].product_id == productId) {
+                productExists = true;
+            }
+        }
+        console.log(productExists);
+        return productExists;
     });
-    if (product) {
-        return true;
-    } else {
-        return false;
-    }
+    
 }
+
+function checkIfImageExists(productId) {
+
+}
+
+function renderProduct(productId) {
+    console.log("я лох");
+    fetch("products.json")
+    .then(response => response.json())
+    .then(json => {
+        for (let i = 0; i < json.products.length; i++) {
+            if (json.products[i].product_id == productId) {
+                document.querySelector("#product-name").innerHTML = json.products[i].name;
+                document.querySelector("#product-price").innerHTML = `${json.products[i].price} грн`;
+                if (json.products[i].description != null) {document.querySelector("#product-info").innerHTML = json.products[i].description;}
+
+                fetch(`images/products/${productId}`, { method: 'HEAD' })
+                .then(res => {
+                    if (res.ok) {
+                        document.querySelector("#product-img").src = `images/products/${productId}/${productId}.png`
+                    } else {
+                        document.querySelector("#product-img").src = `images/promos/promo.png`
+                    }
+                })
+                .catch(err => console.log('Error:', err))
+                
+            }
+        }
+    })
+}
+    
+renderProduct(productId)
