19b4f1e0b5a3cd1e852ad2ef98ddf597c9c63098
Illya <romanenkoisnot@gmail.com>
2023-01-17 19:16:58 +0200

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,30 @@
+function addProductType(productType) {
+    document.getElementById("products").innerHTML = "";
+        fetch("products.json")
+        .then(response => response.json())
+        .then(json => {
+            for (let i = 0; i < json.products.length; i++) {
+                if (json.products[i].product_type == productType) {
+                    addProduct(json.products[i].name, json.products[i].price);
+                }
+            }
+        }
+    );
+}
+
+function addProduct(productName, productPrice) {
+    document.getElementById("products").insertAdjacentHTML("beforeend",
+        `<div class="col-lg-4 col-6 product"><a href="product.html"><img class="promo" src="images/promos/promo3.png" alt="image"></a><p class="product-name">${productName}</p><p class="product-price">${productPrice} грн</p></div>`);
+}
+
+function addAllProducts() {
+    document.getElementById("products").innerHTML = "";
+    fetch("products.json")
+        .then(response => response.json())
+        .then(json => {
+            for (let i = 0; i < json.products.length; i++) {
+                addProduct(json.products[i].name, json.products[i].price);
+            }
+        }
+    );
+}
