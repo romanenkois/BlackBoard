b78f66c65e7fb456f3c471b4df20b448f63d7d57
Illya <romanenkoisnot@gmail.com>
2023-01-19 23:30:45 +0200

    Update shopingCart.js

--- js/shopingCart.js
+++ js/shopingCart.js
@@ -1,28 +1,33 @@
-var buyBtn = document.querySelector("#buy-btn")
+var urlLocation = ((location.href.substring(location.href.lastIndexOf('/') + 1).split("?"))[0])
 
-var productId = (location.href.substring(location.href.lastIndexOf('?') + 1));
+if (urlLocation == "product.html") {
+    var buyBtn = document.querySelector("#buy-btn")
 
-buyBtn.addEventListener("click", function() {
-    existingCart = JSON.parse(localStorage.getItem("shopingCart"));
-    let newCart = []
-    let alredyAdded = false
+    var productId = (location.href.substring(location.href.lastIndexOf('?') + 1));
 
-    if (existingCart != null) {
-        for (let i = 0; i < existingCart.length; i++) {
-            console.log(existingCart[i].product_id)
+    buyBtn.addEventListener("click", function() {
+        existingCart = JSON.parse(localStorage.getItem("shopingCart"));
+        let newCart = []
+        let alredyAdded = false
 
-            if (existingCart[i].product_id == productId) {
-                existingCart[i].product_quantity += 1
-                alredyAdded = true
-                newCart.push(existingCart[i])
-            } else {
-                newCart.push(existingCart[i])
+        if (existingCart != null) {
+            for (let i = 0; i < existingCart.length; i++) {
+                console.log(existingCart[i].product_id)
+
+                if (existingCart[i].product_id == productId) {
+                    existingCart[i].product_quantity += 1
+                    alredyAdded = true
+                    newCart.push(existingCart[i])
+                } else {
+                    newCart.push(existingCart[i])
+                }
             }
+            
         }
-        
-    }
-    if (alredyAdded != true) {
-        newCart.push(({product_id: productId, product_quantity: 1}))
-    }
-    localStorage.setItem("shopingCart", JSON.stringify(newCart))
-})
+        if (alredyAdded != true) {
+            newCart.push(({product_id: productId, product_quantity: 1}))
+        }
+        localStorage.setItem("shopingCart", JSON.stringify(newCart))
+    })
+}
+
