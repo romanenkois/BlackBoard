02a41ee699b46b0178a9153f05904e41d089799e
Illya <romanenkoisnot@gmail.com>
2023-01-20 16:05:57 +0200

    Update navmenu.js

--- js/navmenu.js
+++ js/navmenu.js
@@ -24,6 +24,42 @@ menuBtn.addEventListener("click", function(){
 
 })
 
+function shoppingCartRender() {
+    var shoppingCartData = localStorage.getItem("shoppingCart")
+    document.getElementById("shopping-cart-products").innerHTML = "";
+    console.log(shoppingCartData)
+    if ((shoppingCartData != null)) {
+        shoppingCartData = JSON.parse(shoppingCartData)
+        for (let i = 0; i < shoppingCartData.length; i++) {
+            fetch("products.json")
+            .then(response => response.json())
+            .then(json => {
+                for (let j = 0; j < json.products.length; j++) {
+                    if (shoppingCartData[i].product_id == json.products[j].product_id) {
+                        console.log(shoppingCartData[i])
+                        document.getElementById("shopping-cart-products").insertAdjacentHTML("beforeend",`
+                        <div class="shopping-cart-product row">
+                            <div class="col-3" >
+                                <img style="width: 100%; padding: 5px 0px 5px 0px;" src="images/promos/promo5.png">
+                            </div>
+                            <div class="col-9">
+                                <div class="row">
+                                    <p class="col-12 product-name">${json.products[j].name}</p>
+                                    <p class="col-5 product-name">${shoppingCartData[i].product_quantity} шт</p>
+                                    <p class="col-5 product-price">${json.products[j].price * shoppingCartData[i].product_quantity} грн.</p>
+                                </div>
+                            </div>
+                        </div>
+                        `)
+                    }
+                }
+            })
+        }
+    } else {
+        document.getElementById("shopping-cart-products").insertAdjacentHTML("beforeend","<p class='shopping-cart-product' style='text-align: center' >Поки тут пусто</p>")
+    }
+}
+
 cartBtn.addEventListener("click", function(){
     if (document.querySelector("#shoping-cart").style.display=="block") {
         document.querySelector("#shoping-cart").style.display="none";
@@ -32,5 +68,7 @@ cartBtn.addEventListener("click", function(){
     else {
         document.querySelector("#shoping-cart").style.display="block";
         document.querySelector("#shopping-cart-btn").style.color="var(--light-gray-hover)";
+        shoppingCartRender();
     }
 })
+
