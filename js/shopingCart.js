78029fad5e2074110bd2cda272929e32651210a9
Illya <romanenkoisnot@gmail.com>
2023-01-25 22:11:20 +0200

    Update shopingCart.js

--- js/shopingCart.js
+++ js/shopingCart.js
@@ -1,7 +1,23 @@
 if (location.pathname== "/product.html") {
-    var buyBtn = document.querySelector("#buy-btn")
-
     var productId = (location.href.substring(location.href.lastIndexOf('?') + 1));
+
+    const addBtn = document.querySelector("#add-one");
+    const minusBtn = document.querySelector("#minus-one");
+    const numberToAdd = document.querySelector("#product-number-to-add p");
+    
+    addBtn.addEventListener("click", function() {
+        var oldNumber = parseInt(numberToAdd.innerHTML);
+        numberToAdd.innerHTML = oldNumber + 1;
+    })
+
+    minusBtn.addEventListener("click", function() {
+        var oldNumber = parseInt(numberToAdd.innerHTML);
+        if (oldNumber > 1) {
+            numberToAdd.innerHTML = oldNumber - 1;
+        }
+    })
+
+    const buyBtn = document.querySelector("#buy-btn");
 
     buyBtn.addEventListener("click", function() {
         existingCart = JSON.parse(localStorage.getItem("shoppingCart"));
@@ -12,21 +28,22 @@ if (location.pathname== "/product.html") {
             for (let i = 0; i < existingCart.length; i++) {
 
                 if (existingCart[i].product_id == productId) {
-                    existingCart[i].product_quantity += 1
-                    alredyAdded = true
-                    newCart.push(existingCart[i])
+                    if (parseInt(numberToAdd.innerHTML) > 0){
+                        existingCart[i].product_quantity += parseInt(numberToAdd.innerHTML);
+                        alredyAdded = true;
+                        newCart.push(existingCart[i]);
+                    }
                 } else {
                     newCart.push(existingCart[i])
                 }
             }
         }
         if (alredyAdded != true) {
-            newCart.push(({product_id: productId, product_quantity: 1}))
+            newCart.push(({product_id: productId, product_quantity: parseInt(numberToAdd.innerHTML)}));
         }
-        localStorage.setItem("shoppingCart", JSON.stringify(newCart))
+        localStorage.setItem("shoppingCart", JSON.stringify(newCart));
         if (document.querySelector("#shoping-cart").style.display == "block") {
-            shoppingCartRender()
+            shoppingCartRender();
         }
     })
 }
-
