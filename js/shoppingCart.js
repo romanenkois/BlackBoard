b0eaa26d5a495d2f0dbcfb99c390b1c422817f49
Illya <romanenkoisnot@gmail.com>
2023-02-02 18:03:44 +0200

    Update shoppingCart.js

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,11 +1,9 @@
-
 function checkIfCartEmpty() {
     let shoppingCartData = localStorage.getItem("shoppingCart")
-    console.log("бля")
     if ((shoppingCartData != null) && (shoppingCartData != "[]")) {
-        console.log("мурмур")
     } else {
         location.href = "shoppingCartEmpty.html"
     }
 }
+
 checkIfCartEmpty()
